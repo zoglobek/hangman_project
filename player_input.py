01ce09b74811dc460a7be5dc5d2d6f1484656845
zoglobek <145182958+zoglobek@users.noreply.github.com>
2025-11-09 20:33:59 +0200

    made the call to check _if func
    need to build check if

--- player_input.py
+++ player_input.py
@@ -2,7 +2,7 @@ def player_guess():
     guess = str(input("Please input a letter"))
     guess = guess.lower()
     if len(guess) == 1:
-
+        check_if()
     else:
         print("Invalid guess")
         print("guess again")
