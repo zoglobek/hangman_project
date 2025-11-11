f5adef93dcf3867b6575ccfaa8b2ef36d455e617
gal <newmangal@msn.com>
2025-11-11 19:59:16 +0200

    bug in guess line showing before also import problem with hangman_pics

--- main_game.py
+++ main_game.py
@@ -1,3 +1,4 @@
+from click import clear
 import time
 from hangman_project.hanged_pics import hangmanpics, hanged_openning
 from hangman_project.large_list import guessing_line
@@ -5,11 +6,12 @@ from hangman_project.player_input import player_guess
 def greet ():
     print(hanged_openning)
     PNAME = input("Please enter your name\n").capitalize()
-    print(f"\n\n Hi {PNAME} Welcome to the Hangman's game!!!")
+    print(f"\n\nHi {PNAME} \nWelcome to the Hangman's game!!!")
 
 if __name__ == '__main__':
     greet()
+    time.sleep(3)
+    clear()
     player_guess()
-    sleep(5)
     print(guessing_line)
 
