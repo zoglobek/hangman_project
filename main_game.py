074a880c652b2c04ca306bc3677b2e57674eb6c6
gal <newmangal@msn.com>
2025-11-11 17:42:10 +0200

    rebuilt main

--- main_game.py
+++ main_game.py
@@ -1,14 +1,15 @@
-import random
-from hangman_project.large_list import dotted_line
+import time
+from hangman_project.hanged_pics import hangmanpics, hanged_openning
+from hangman_project.large_list import guessing_line
 from hangman_project.player_input import player_guess
-PNAME = input("Please enter your name\n").capitalize()
-greet = print(f"Hi {PNAME} Welcome to the Hangman's game!!!")
-
-
+def greet ():
+    print(hanged_openning)
+    PNAME = input("Please enter your name\n").capitalize()
+    print(f"\n\n Hi {PNAME} Welcome to the Hangman's game!!!")
 
 if __name__ == '__main__':
-    greet
-    print(dotted_line())
+    greet()
     player_guess()
-    print()
+    sleep(5)
+    print(guessing_line)
 
